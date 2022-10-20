a3215e7309c63af44bd57fa2fce342dd95ddb7bc
Puroblast <Puroblast97@mail.ru>
2022-10-21 00:04:39 +0300

    reworked multiply, now its working properly

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -8,9 +8,9 @@ import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by viewBinding()
-    private var memory: Int = 0
-    private var firstNumber = 0
+    private var memory = 0
     private var lastFunction = ""
+    private var equalpressed = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -41,8 +41,15 @@ class MainActivity : AppCompatActivity() {
 
         }
         binding.fiveButton.setOnClickListener {
+            if (equalpressed) {
+                binding.totalView.text = ""
+                equalpressed = false
+            }
+            if(lastFunction != ""){
+                binding.totalView.text = ""
+            }
             binding.totalView.append("5")
-            if(lastFunction == "") {
+            if (lastFunction == ""){
                 renew()
             }
 
@@ -77,9 +84,10 @@ class MainActivity : AppCompatActivity() {
 
         }
         binding.multButton.setOnClickListener {
-            renew()
+            if (lastFunction == "multiply") {
+                equal()
+            }
             lastFunction = "multiply"
-            binding.totalView.text = "0"
         }
         binding.minusButton.setOnClickListener {
             renew()
@@ -97,14 +105,22 @@ class MainActivity : AppCompatActivity() {
             equal()
         }
         binding.clearButton.setOnClickListener {
-            binding.totalView.text = "0"
+            binding.totalView.text = ""
+            equalpressed = false
+            lastFunction = ""
         }
     }
 
     private fun equal() {
+        equalpressed = true
         when(lastFunction) {
 
-            "multiply" -> binding.totalView.text = multiply().toString()
+            "multiply" -> {
+                multiply()
+                binding.totalView.text = memory.toString()
+                lastFunction = ""
+                renew()
+            }
             "divide" -> divide()
             "plus" -> plus()
             "minus" -> minus()
@@ -113,12 +129,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun renew() {
-        memory = binding.totalView.text.toString().toInt()
+        if (binding.totalView.text != "") {
+            memory = binding.totalView.text.toString().toInt()
+        }
+
     }
 
-    private fun multiply() : Int {
-        memory = memory * binding.totalView.text.toString().toInt()
-        return memory
+    private fun multiply() {
+        if (equalpressed) {
+            memory = memory * binding.totalView.text.toString().toInt()
+        } else {
+            equal()
+        }
+
     }
 
     private fun divide() {
